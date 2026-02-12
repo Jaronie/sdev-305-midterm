eede3773b945b5b4dc3a2043cdf1ce5949e3ada9
Jaron <jayisaac6706@gmail.com>
2026-02-11 23:31:45 -0800

    app.js routes and PORT created

---
+++ app.js
@@ -0,0 +1,29 @@
+//import express
+import express from 'express';
+
+//create instance for express
+const app = express();
+
+
+//define port number 3001
+const PORT = 3001;
+
+//enable static serving for public
+app.use(express.static('public'));
+
+
+// default route
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/views/home.html`);
+});
+
+// thank you route after form submission
+app.get('/thank-you', (req, res) => {
+    res.sendFile(`${__dirname}/views/confirmation.html`);
+});
+
+
+// start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
